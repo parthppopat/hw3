c76f6d1b42a1fa8fee698038c15d0c95e33890a1
Parth Popat <parth.popat@kellogg.northwestern.edu>
2021-06-03 16:14:35 -0500

    new chnages

--- noober.js
+++ noober.js
@@ -1,122 +1,50 @@
-let levelOfService
-let passenger1Name
-let passenger1Phone
-let passenger1NumberOfPassengers
-let passenger1PickupAddressLine1
-let passenger1PickupAddressLine2
-let passenger1DropoffAddressLine1
-let passenger1DropoffAddressLine2
-let passenger2Name
-let passenger2Phone
-let passenger2NumberOfPassengers
-let passenger2PickupAddressLine1
-let passenger2PickupAddressLine2
-let passenger2DropoffAddressLine1
-let passenger2DropoffAddressLine2
-let passenger3Name
-let passenger3Phone
-let passenger3NumberOfPassengers
-let passenger3PickupAddressLine1
-let passenger3PickupAddressLine2
-let passenger3DropoffAddressLine1
-let passenger3DropoffAddressLine2
+// Code that runs when the page finishes loading
+document.addEventListener('DOMContentLoaded', async function() {
+  // retrieves data from rides API endpoint
+  let response = await fetch('https://kiei451.com/api/rides.json')
+  let json = await response.json()
 
-function pageLoad() {
-  // 👇 YOUR CODE BEGINS HERE. DON'T CHANGE ANY OTHER CODE. 👇
+  // selects a random ride
+  let ride = json[Math.floor(Math.random() * json.length)]
 
-  // 🔥🔥🔥 YOUR CODE GOES HERE 🔥🔥🔥
-  // the 'ride' variable contains data about a random ride (i.e. when the page)
-  // is refreshed or reloaded, the 'ride' variable will contain something different
-  // to get started, try to write other parts of 'ride' to the JavaScript console in Chrome
-  // e.g. ask 'ride' for its length or only grab the first member of the Array
+  // writes random ride to the JavaScript console
   console.log(ride)
 
-  // these variables map to the elements on the finished page;
-  // assign the proper value to each variable
-
-  numberOfRides = ride.length
-
-  
-  if (numberOfRides > 1) {
-    levelOfService = 'Noober Pool'
-
-
-    if (numberOfRides == 2) {
-      // Passenger 1 details
-      passenger1Name = ride[0].passengerDetails.first + ' ' + ride[0].passengerDetails.last
-      passenger1Phone = ride[0].passengerDetails.phoneNumber
-      passenger1NumberOfPassengers = ride[0].numberOfPassengers
-      passenger1PickupAddressLine1 = ride[0].pickupLocation.address
-      passenger1PickupAddressLine2 = ride[0].pickupLocation.city + ', ' + ride[0].pickupLocation.state + ' ' + ride[0].pickupLocation.zip
-      passenger1DropoffAddressLine1 = ride[0].dropoffLocation.address
-      passenger1DropoffAddressLine2 = ride[0].dropoffLocation.city + ', ' + ride[0].dropoffLocation.state + ' ' + ride[0].dropoffLocation.zip
-
-      // Passenger 2 details
-      passenger2Name = ride[1].passengerDetails.first + ' ' + ride[1].passengerDetails.last
-      passenger2Phone = ride[1].passengerDetails.phoneNumber
-      passenger2NumberOfPassengers = ride[1].numberOfPassengers
-      passenger2PickupAddressLine1 = ride[1].pickupLocation.address
-      passenger2PickupAddressLine2 = ride[1].pickupLocation.city + ', ' + ride[1].pickupLocation.state + ' ' + ride[1].pickupLocation.zip
-      passenger2DropoffAddressLine1 = ride[1].dropoffLocation.address
-      passenger2DropoffAddressLine2 = ride[1].dropoffLocation.city + ', ' + ride[1].dropoffLocation.state + ' ' + ride[1].dropoffLocation.zip
-
-    } else if (numberOfRides == 3) {
-      // Passenger 1 details
-      passenger1Name = ride[0].passengerDetails.first + ' ' + ride[0].passengerDetails.last
-      passenger1Phone = ride[0].passengerDetails.phoneNumber
-      passenger1NumberOfPassengers = ride[0].numberOfPassengers
-      passenger1PickupAddressLine1 = ride[0].pickupLocation.address
-      passenger1PickupAddressLine2 = ride[0].pickupLocation.city + ', ' + ride[0].pickupLocation.state + ' ' + ride[0].pickupLocation.zip
-      passenger1DropoffAddressLine1 = ride[0].dropoffLocation.address
-      passenger1DropoffAddressLine2 = ride[0].dropoffLocation.city + ', ' + ride[0].dropoffLocation.state + ' ' + ride[0].dropoffLocation.zip
-
-      // Passenger 2 details
-      passenger2Name = ride[1].passengerDetails.first + ' ' + ride[1].passengerDetails.last
-      passenger2Phone = ride[1].passengerDetails.phoneNumber
-      passenger2NumberOfPassengers = ride[1].numberOfPassengers
-      passenger2PickupAddressLine1 = ride[1].pickupLocation.address
-      passenger2PickupAddressLine2 = ride[1].pickupLocation.city + ', ' + ride[1].pickupLocation.state + ' ' + ride[1].pickupLocation.zip
-      passenger2DropoffAddressLine1 = ride[1].dropoffLocation.address
-      passenger2DropoffAddressLine2 = ride[1].dropoffLocation.city + ', ' + ride[1].dropoffLocation.state + ' ' + ride[1].dropoffLocation.zip
-
-      // Passenger 3 details
-      passenger3Name = ride[2].passengerDetails.first + ' ' + ride[2].passengerDetails.last
-      passenger3Phone = ride[2].passengerDetails.phoneNumber
-      passenger3NumberOfPassengers = ride[2].numberOfPassengers
-      passenger3PickupAddressLine1 = ride[2].pickupLocation.address
-      passenger3PickupAddressLine2 = ride[2].pickupLocation.city + ', ' + ride[2].pickupLocation.state + ' ' + ride[2].pickupLocation.zip
-      passenger3DropoffAddressLine1 = ride[2].dropoffLocation.address
-      passenger3DropoffAddressLine2 = ride[2].dropoffLocation.city + ', ' + ride[2].dropoffLocation.state + ' ' + ride[2].dropoffLocation.zip
-    }
+// 🔥 YOUR CODE GOES HERE 🔥
+// Write the recipe (algorithm) in the comments. Then, write the code.
+// Find the kind of car passenger wants
+// Since Noober Purple overrides all other ride types, first check if it is Noober Purple 
+  if (ride.purpleRequested == true) {
+// If the above is true, we will provide output rideType as Noober Purple
+    rideType = `Noober Purple`
+// Check whether there are more than 3 passengers if it is not an Noober Purple
+  } else if (ride.numberOfPassengers > 3) {
+// If there are > 3 passengers, we will output rideType as Noober XL
+    rideType = `Noober XL`
+// We will default to Noober X if it is neither Noober Purple nor > 3 passengers
   } else {
-    passenger1Name = ride[0].passengerDetails.first + ' ' + ride[0].passengerDetails.last
-    passenger1Phone = ride[0].passengerDetails.phoneNumber
-    passenger1NumberOfPassengers = ride[0].numberOfPassengers
-    passenger1PickupAddressLine1 = ride[0].pickupLocation.address
-    passenger1PickupAddressLine2 = ride[0].pickupLocation.city + ', ' + ride[0].pickupLocation.state + ' ' + ride[0].pickupLocation.zip
-    passenger1DropoffAddressLine1 = ride[0].dropoffLocation.address
-    passenger1DropoffAddressLine2 = ride[0].dropoffLocation.city + ', ' + ride[0].dropoffLocation.state + ' ' + ride[0].dropoffLocation.zip
-      
+    rideType = `Noober X`
+  }
+// We will now extract passenger's first and last name from their profile and store it in memory
+  let passengerFirstname = ride.passengerDetails.first
+  let passengerLastname = ride.passengerDetails.last
+// We will now extract passenger's first and last nameand store it in memory
+  let passengerPhone = ride.passengerDetails.phoneNumber
+// We will now extract passenger's address details and store the text in pickupDetails
+  let location = ride.pickupLocation
+  let pickupDetails = `${location.address}, ${location.city}, ${location.state}, ${location.zip}`
+// We will now extract passenger's dropoff location and store the text in dropoffDetails
+let dropLocation = ride.dropoffLocation
+let dropoffDetails = `${dropLocation.address}, ${dropLocation.city}, ${dropLocation.state}, ${dropLocation.zip}`
+// We will now infrom the driver of  the number of passengers
+let passengerAmount = ride.numberOfPassengers
+// We will now get the full details of the ride displayed on the console
+  console.log(`Ride type: ${rideType}
+  ${rideType} Passenger: ${passengerFirstname} ${passengerLastname}
+  Pickup at: ${pickupDetails}
+  Dropoff at: ${dropoffDetails}
+  Expected number of riders: ${passengerAmount}
+  Phone: ${passengerPhone}`)
 
-    if (ride[0].purpleRequested == true) {
-      levelOfService = 'Noober Purple'
-    } else if (ride[0].numberOfPassengers > 3) {
-        levelOfService = 'Noober XL'
-    } else {
-        levelOfService = 'Noober X'
-    }
-    }
-
-    // levelOfService = 'tacos'
-  // passenger1Name, passenger1Phone, passenger1NumberOfPassengers
-  // passenger1PickupAddressLine1, passenger1PickupAddressLine2
-  // passenger1DropoffAddressLine1, passenger1DropoffAddressLine2
-  // passenger2Name, passenger2Phone, passenger2NumberOfPassengers
-  // passenger2PickupAddressLine1, passenger2PickupAddressLine2
-  // passenger2DropoffAddressLine1, passenger2DropoffAddressLine2
-  // passenger3Name, passenger3Phone, passenger3NumberOfPassengers
-  // passenger3PickupAddressLine1, passenger3PickupAddressLine2
-  // passenger3DropoffAddressLine1, passenger3DropoffAddressLine2
-  
-  //  👆 YOUR CODE ENDS HERE. DON'T CHANGE ANY OTHER CODE 👆
-}
+// 🔥 YOUR CODE ENDS HERE 🔥
+})
